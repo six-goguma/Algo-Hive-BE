a1ccfb778edf2428c17d9dedd191c3242f1f2f17
gitjiho <jiho99322@gmail.com>
2025-01-03 22:15:39 +0900

    set: start project

---
+++ algo-hive/src/main/java/com/knu/algo_hive/AlgoHiveApplication.java
@@ -0,0 +1,13 @@
+package com.knu.algo_hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoHiveApplication.class, args);
+	}
+
+}
---
+++ algo-hive/src/test/java/com/knu/algo_hive/AlgoHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.algo_hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
