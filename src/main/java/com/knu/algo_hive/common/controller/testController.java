f9954ea6e9b775297c81c27bbb736721f0a4b130
gitjiho <jiho99322@gmail.com>
2025-01-05 22:41:47 +0900

    fix: delete 로직 수정:

--- src/main/java/com/knu/algo_hive/common/controller/testController.java
+++ src/main/java/com/knu/algo_hive/common/controller/testController.java
@@ -9,11 +9,11 @@ public class testController {
 
     @GetMapping("/api")
     public ResponseEntity<String> mainPage() {
-        return ResponseEntity.ok().body("test8");
+        return ResponseEntity.ok().body("test10");
     }
 
     @GetMapping("/api/oh")
     public ResponseEntity<String> sidePage() {
-        return ResponseEntity.ok().body("new test9");
+        return ResponseEntity.ok().body("new test10");
     }
 }
