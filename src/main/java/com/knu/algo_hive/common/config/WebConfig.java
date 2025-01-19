88c592b9afc2e9ca6a2e216d42855a30683a2c42
gitjiho <jiho99322@gmail.com>
2025-01-20 00:58:17 +0900

    hotfix: allowCredentials(false)

--- src/main/java/com/knu/algo_hive/common/config/WebConfig.java
+++ src/main/java/com/knu/algo_hive/common/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
