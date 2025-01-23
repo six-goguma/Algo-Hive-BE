a47af200c2610d734a8365ec678f41fcae89947e
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-23 17:22:06 +0900

    feat: 게시물 관련 API 구현

--- src/main/java/com/knu/algo_hive/post/controller/PostController.java
+++ src/main/java/com/knu/algo_hive/post/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.knu.algo_hive.post.controller;
 
+import com.knu.algo_hive.auth.service.CustomUserDetails;
 import com.knu.algo_hive.post.dto.PostRequest;
 import com.knu.algo_hive.post.dto.PostResponse;
 import com.knu.algo_hive.post.dto.PostSummaryResponse;
+import com.knu.algo_hive.post.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -10,21 +12,24 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @Tag(name = "게시물_CRUD", description = "게시물 관련 API")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/api/v1/posts")
-    @Operation(summary = "모든 게시물 페이지네이션 조회(미구현)",
+    @Operation(summary = "모든 게시물 페이지네이션 조회",
             description = "페이지네이션 적용. /api/v1/posts/all?page={page번호}&size={page content 개수}&sort={content 속성},{desc || asc} ✅ex)좋아요 개수 기준 내림차순 조회 /api/v1/posts/all?page=0&size=10&sort=likeCount,desc"
     )
     @Parameters({
@@ -33,17 +38,13 @@ public class PostController {
             @Parameter(in = ParameterIn.QUERY, name = "sort", description = "정렬 기준 (속성,오름차순|내림차순)", example = "createdAt,desc", schema = @Schema(type = "string"))
     })
     public ResponseEntity<Page<PostSummaryResponse>> getPostSummaries(@Parameter(hidden = true)
-                                                                      @PageableDefault(size = 10, sort = "createdAt,desc")
+                                                                      @PageableDefault(sort = "createdAt,desc")
                                                                       Pageable pageable) {
-        PostSummaryResponse post1 = new PostSummaryResponse(1L, "굉장한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약!", LocalDateTime.now(), 777, 777, "김계란");
-        PostSummaryResponse post2 = new PostSummaryResponse(0L, "이상한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약~", LocalDateTime.now().minusDays(3), 3, 53, "김김");
-        List<PostSummaryResponse> posts = List.of(post1, post2);
-        Page<PostSummaryResponse> page = new PageImpl<>(posts, PageRequest.of(0, 5), 2);
-        return ResponseEntity.ok(page);
+        return ResponseEntity.ok(postService.getPostSummaries(pageable));
     }
 
     @GetMapping("/api/v1/{nickname}/posts")
-    @Operation(summary = "특정 유저의 게시물 페이지네이션 조회(미구현)",
+    @Operation(summary = "특정 유저의 게시물 페이지네이션 조회",
             description = "페이지네이션 적용. /api/v1/posts?page={page번호}&size={page content 개수}&sort={content 속성},{desc || asc} ✅ex)좋아요 개수 기준 내림차순 조회 /api/v1/posts?page=0&size=10&sort=likeCount,desc"
 
     )
@@ -52,53 +53,54 @@ public class PostController {
             @Parameter(in = ParameterIn.QUERY, name = "size", description = "페이지 크기", example = "10", schema = @Schema(type = "integer", defaultValue = "10")),
             @Parameter(in = ParameterIn.QUERY, name = "sort", description = "정렬 기준 (속성,오름차순|내림차순)", example = "createdAt,desc", schema = @Schema(type = "string"))
     })
-    public ResponseEntity<Page<PostSummaryResponse>> getMyPostSummaries(@Parameter(hidden = true)
-                                                                        @PageableDefault(size = 10, sort = "createdAt,desc")
-                                                                        Pageable pageable,
-                                                                        @PathVariable String nickname) {
-        PostSummaryResponse post1 = new PostSummaryResponse(1L, "굉장한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약!", LocalDateTime.now(), 777, 777, "아무개");
-        PostSummaryResponse post2 = new PostSummaryResponse(0L, "이상한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약~", LocalDateTime.now().minusDays(3), 3, 53, "홍길동");
-        List<PostSummaryResponse> posts = List.of(post1, post2);
-        Page<PostSummaryResponse> page = new PageImpl<>(posts, PageRequest.of(0, 5), 2);
-        return ResponseEntity.ok(page);
+    public ResponseEntity<Page<PostSummaryResponse>> getPostSummariesByNickname(@Parameter(hidden = true)
+                                                                                @PageableDefault(sort = "createdAt,desc")
+                                                                                Pageable pageable,
+                                                                                @PathVariable String nickname) {
+        return ResponseEntity.ok(postService.getPostSummariesByNickname(pageable, nickname));
     }
 
     @GetMapping("/api/v1/posts/{postId}")
-    @Operation(summary = "게시물 상세 조회(미구현)",
+    @Operation(summary = "게시물 상세 조회",
             description = "게시물 상세 조회"
     )
     public ResponseEntity<PostResponse> getPost(@PathVariable Long postId) {
-        PostResponse postResponse = new PostResponse(0L, "굉장한 제목", "Hello 방가", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약!", 777, 777, LocalDateTime.now(), LocalDateTime.now(), "JAMES");
-        return ResponseEntity.ok(postResponse);
+        return ResponseEntity.ok(postService.getPost(postId));
     }
 
     @PostMapping("/api/v1/posts")
-    @Operation(summary = "게시물 저장(미구현)",
+    @Operation(summary = "게시물 저장",
             description = "게시물 저장"
     )
-    public ResponseEntity<Void> savePost(@RequestBody PostRequest request) {
+    public ResponseEntity<Void> savePost(@RequestBody PostRequest request,
+                                         @AuthenticationPrincipal CustomUserDetails userDetails) {
+        postService.savePost(request, userDetails.getUsername());
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/api/v1/posts/{postId}")
-    @Operation(summary = "게시물 수정(미구현)",
+    @Operation(summary = "게시물 수정",
             description = "게시물 수정"
     )
     public ResponseEntity<Void> updatePost(@PathVariable Long postId,
-                                           @RequestBody PostRequest request) {
+                                           @RequestBody PostRequest request,
+                                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+        postService.updatePost(postId, request, userDetails.getUsername());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/api/v1/posts/{postId}")
-    @Operation(summary = "게시물 삭제(미구현)",
+    @Operation(summary = "게시물 삭제",
             description = "게시물 삭제"
     )
-    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId,
+                                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+        postService.deletePost(postId, userDetails.getUsername());
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/api/v1/posts/tags/{tagName}")
-    @Operation(summary = " 태그별로 모든 게시물 페이지네이션 조회(미구현)",
+    @Operation(summary = " 태그별로 모든 게시물 페이지네이션 조회",
             description = "페이지네이션 적용. /api/v1/posts?page={page번호}&size={page content 개수}&sort={content 속성},{desc || asc} ✅ex)좋아요 개수 기준 내림차순 조회 /api/v1/posts?page=0&size=10&sort=likeCount,desc"
 
     )
@@ -108,19 +110,14 @@ public class PostController {
             @Parameter(in = ParameterIn.QUERY, name = "sort", description = "정렬 기준 (속성,오름차순|내림차순)", example = "createdAt,desc", schema = @Schema(type = "string"))
     })
     public ResponseEntity<Page<PostSummaryResponse>> getAllPostSummariesByTag(@Parameter(hidden = true)
-                                                                              @PageableDefault(size = 10, sort = "createdAt,desc")
+                                                                              @PageableDefault(sort = "createdAt,desc")
                                                                               @PathVariable String tagName,
                                                                               Pageable pageable) {
-
-        PostSummaryResponse post1 = new PostSummaryResponse(1L, "굉장한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약!", LocalDateTime.now(), 777, 777, "이름이다");
-        PostSummaryResponse post2 = new PostSummaryResponse(0L, "이상한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약~", LocalDateTime.now().minusDays(3), 3, 53, "저자는 나");
-        List<PostSummaryResponse> posts = List.of(post1, post2);
-        Page<PostSummaryResponse> page = new PageImpl<>(posts, PageRequest.of(0, 5), 2);
-        return ResponseEntity.ok(page);
+        return ResponseEntity.ok(postService.getAllPostSummariesByTag(tagName, pageable));
     }
 
     @GetMapping("/api/v1/{nickname}/posts/tags/{tagName}")
-    @Operation(summary = "특정 유저의 태그별 게시물 페이지네이션 조회(미구현)",
+    @Operation(summary = "특정 유저의 태그별 게시물 페이지네이션 조회",
             description = "페이지네이션 적용. /api/v1/posts?page={page번호}&size={page content 개수}&sort={content 속성},{desc || asc} ✅ex)좋아요 개수 기준 내림차순 조회 /api/v1/posts?page=0&size=10&sort=likeCount,desc"
 
     )
@@ -130,15 +127,11 @@ public class PostController {
             @Parameter(in = ParameterIn.QUERY, name = "sort", description = "정렬 기준 (속성,오름차순|내림차순)", example = "createdAt,desc", schema = @Schema(type = "string"))
     })
     public ResponseEntity<Page<PostSummaryResponse>> getPostSummariesByTag(@Parameter(hidden = true)
-                                                                           @PageableDefault(size = 10, sort = "createdAt,desc")
+                                                                           @PageableDefault(sort = "createdAt,desc")
                                                                            @PathVariable String tagName,
-                                                                           Pageable pageable,
-                                                                           @PathVariable String nickname) {
-
-        PostSummaryResponse post1 = new PostSummaryResponse(1L, "굉장한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약!", LocalDateTime.now(), 777, 777, "방가");
-        PostSummaryResponse post2 = new PostSummaryResponse(0L, "이상한 제목", "https://cdn.pixabay.com/photo/2024/02/12/21/34/sunset-8569636_1280.jpg", "요약~", LocalDateTime.now().minusDays(3), 3, 53, "하하");
-        List<PostSummaryResponse> posts = List.of(post1, post2);
-        Page<PostSummaryResponse> page = new PageImpl<>(posts, PageRequest.of(0, 5), 2);
-        return ResponseEntity.ok(page);
+                                                                           @PathVariable String nickname,
+                                                                           Pageable pageable
+    ) {
+        return ResponseEntity.ok(postService.getPostSummariesByTag(tagName, nickname, pageable));
     }
 }
---
+++ src/main/java/com/knu/algo_hive/post/entity/Post.java
@@ -0,0 +1,50 @@
+package com.knu.algo_hive.post.entity;
+
+import com.knu.algo_hive.auth.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String content;
+    private String thumbnail;
+    private String summary;
+    private int likeCount = 0;
+    private int commentCount = 0;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "post_tag",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    Set<Tag> tags = new HashSet<>();
+
+    public Post(String content, String summary, String thumbnail, String title, Member member) {
+        this.content = content;
+        this.summary = summary;
+        this.thumbnail = thumbnail;
+        this.title = title;
+        this.member = getMember();
+    }
+}
