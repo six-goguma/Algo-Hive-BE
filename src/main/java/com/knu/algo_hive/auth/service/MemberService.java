30a6045758f2fc30e129b0ee0a98fde5bbb6cf42
2iedo <162278702+2iedo@users.noreply.github.com>
2025-01-31 18:13:40 +0900

    feat : 로그인 / 회원가입 성공 시 사용자 정보 리턴

--- src/main/java/com/knu/algo_hive/auth/service/MemberService.java
+++ src/main/java/com/knu/algo_hive/auth/service/MemberService.java
@@ -1,10 +1,12 @@
 package com.knu.algo_hive.auth.service;
 
 import com.knu.algo_hive.auth.dto.LoginRequest;
+import com.knu.algo_hive.auth.dto.LoginResponse;
 import com.knu.algo_hive.auth.dto.RegisterRequest;
 import com.knu.algo_hive.auth.entity.Member;
 import com.knu.algo_hive.auth.repository.MemberRepository;
 import com.knu.algo_hive.common.exception.BadRequestException;
+import com.knu.algo_hive.common.exception.ConflictException;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,6 +21,7 @@ import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -38,7 +41,8 @@ public class MemberService {
     private final long ACCESS_THREE_MINUTES = 1000 * 60 * 3;
     private final long ACCESS_TEN_MINUTES = 1000 * 60 * 10;
 
-    public void register(RegisterRequest registerRequest) {
+    @Transactional
+    public LoginResponse register(RegisterRequest registerRequest) {
         checkEmail(registerRequest.email());
         checkNickName(registerRequest.nickName());
 
@@ -50,9 +54,11 @@ public class MemberService {
                 passwordEncoder.encode(registerRequest.password()));
 
         memberRepository.save(member);
+        return new LoginResponse(member.getNickName());
     }
 
-    public void login(LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
+    @Transactional(readOnly = true)
+    public LoginResponse login(LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
 
@@ -61,14 +67,21 @@ public class MemberService {
         SecurityContext context = this.securityContextHolderStrategy.createEmptyContext();
         context.setAuthentication(authentication);
         securityContextRepository.saveContext(context, request, response);
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Member member = userDetails.getMember();
+
+        return new LoginResponse(member.getNickName());
     }
 
+    @Transactional(readOnly = true)
     public void checkEmail(String email) {
-        if (memberRepository.existsByEmail(email)) throw new BadRequestException("중복된 이메일이 있습니다.");
+        if (memberRepository.existsByEmail(email)) throw new ConflictException("중복된 이메일이 있습니다.");
     }
 
+    @Transactional(readOnly = true)
     public void checkNickName(String nickname) {
-        if (memberRepository.existsByNickName(nickname)) throw new BadRequestException("중복된 닉네임이 있습니다.");
+        if (memberRepository.existsByNickName(nickname)) throw new ConflictException("중복된 닉네임이 있습니다.");
     }
 
     public void postCode(String email) throws MessagingException {
