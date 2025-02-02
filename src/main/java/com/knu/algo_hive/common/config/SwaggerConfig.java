370baa32d87f3c9157dc0d8f9fc7ebcea9bb32aa
2iedo <162278702+2iedo@users.noreply.github.com>
2025-02-02 18:57:02 +0900

    feat : swagger에 에러 코드 커스텀 부분 추가

--- src/main/java/com/knu/algo_hive/common/config/SwaggerConfig.java
+++ src/main/java/com/knu/algo_hive/common/config/SwaggerConfig.java
@@ -1,10 +1,28 @@
 package com.knu.algo_hive.common.config;
 
+import com.knu.algo_hive.common.annotation.ApiErrorCodeExample;
+import com.knu.algo_hive.common.annotation.ApiErrorCodeExamples;
+import com.knu.algo_hive.common.exception.ErrorCode;
+import com.knu.algo_hive.common.exception.ErrorResponseDto;
+import com.knu.algo_hive.common.exception.ExampleHolder;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.examples.Example;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,5 +39,93 @@ public class SwaggerConfig {
                 .title("Algo-Hive Team Toy Project 명세서")
                 .description("[Team Notion 바로가기](https://www.notion.so/1602c5ccabe480a4821bdbf72f003c57)")
                 .version("0.0.1");
+    }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            ApiErrorCodeExamples apiErrorCodeExamples = handlerMethod.getMethodAnnotation(
+                    ApiErrorCodeExamples.class);
+
+            if (apiErrorCodeExamples != null) {
+                generateErrorCodeResponseExample(operation, apiErrorCodeExamples.value());
+            } else {
+                ApiErrorCodeExample apiErrorCodeExample = handlerMethod.getMethodAnnotation(
+                        ApiErrorCodeExample.class);
+
+                if (apiErrorCodeExample != null) {
+                    generateErrorCodeResponseExample(operation, apiErrorCodeExample.value());
+                }
+            }
+
+            return operation;
+        };
+    }
+
+    private void generateErrorCodeResponseExample(Operation operation, ErrorCode[] errorCodes) {
+        ApiResponses responses = operation.getResponses();
+
+        Map<Integer, List<ExampleHolder>> statusWithExampleHolders = Arrays.stream(errorCodes)
+                .map(
+                        errorCode -> ExampleHolder.builder()
+                                .holder(getSwaggerExample(errorCode))
+                                .code(errorCode.getHttpStatus().value())
+                                .name(errorCode.name())
+                                .build()
+                )
+                .collect(Collectors.groupingBy(ExampleHolder::getCode));
+
+        addExamplesToResponses(responses, statusWithExampleHolders);
+    }
+
+    private void generateErrorCodeResponseExample(Operation operation, ErrorCode errorCode) {
+        ApiResponses responses = operation.getResponses();
+
+        ExampleHolder exampleHolder = ExampleHolder.builder()
+                .holder(getSwaggerExample(errorCode))
+                .name(errorCode.name())
+                .code(errorCode.getHttpStatus().value())
+                .build();
+        addExamplesToResponses(responses, exampleHolder);
+    }
+
+    private Example getSwaggerExample(ErrorCode errorCode) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(errorCode);
+        Example example = new Example();
+        example.setValue(errorResponseDto);
+
+        return example;
+    }
+
+    private void addExamplesToResponses(ApiResponses responses,
+                                        Map<Integer, List<ExampleHolder>> statusWithExampleHolders) {
+        statusWithExampleHolders.forEach(
+                (status, v) -> {
+                    Content content = new Content();
+                    MediaType mediaType = new MediaType();
+                    ApiResponse apiResponse = new ApiResponse();
+
+                    v.forEach(
+                            exampleHolder -> mediaType.addExamples(
+                                    exampleHolder.getName(),
+                                    exampleHolder.getHolder()
+                            )
+                    );
+                    content.addMediaType("application/json", mediaType);
+                    apiResponse.setContent(content);
+                    responses.addApiResponse(String.valueOf(status), apiResponse);
+                }
+        );
+    }
+
+    private void addExamplesToResponses(ApiResponses responses, ExampleHolder exampleHolder) {
+        Content content = new Content();
+        MediaType mediaType = new MediaType();
+        ApiResponse apiResponse = new ApiResponse();
+
+        mediaType.addExamples(exampleHolder.getName(), exampleHolder.getHolder());
+        content.addMediaType("application/json", mediaType);
+        apiResponse.content(content);
+        responses.addApiResponse(String.valueOf(exampleHolder.getCode()), apiResponse);
     }
 }
