00517c4aa6d5abd7efe2db52d25952b1f994e05f
2iedo <162278702+2iedo@users.noreply.github.com>
2025-02-02 18:56:30 +0900

    feat : ErrorCode를 인자로 받는 생성자 추가

--- src/main/java/com/knu/algo_hive/common/exception/UnauthorizedException.java
+++ src/main/java/com/knu/algo_hive/common/exception/UnauthorizedException.java
@@ -2,7 +2,9 @@ package com.knu.algo_hive.common.exception;
 
 public class UnauthorizedException extends RuntimeException {
 
+    ErrorCode errorCode;
     public UnauthorizedException(String message) {
         super(message);
     }
+    public UnauthorizedException(ErrorCode errorcode){this.errorCode = errorcode;}
 }
