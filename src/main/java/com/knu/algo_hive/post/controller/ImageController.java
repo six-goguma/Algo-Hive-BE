afc1f8c2eb0791b7e2ff40767ccc9f6d7c8192a8
Seonghun Jeong <tjdgns5506@gmail.com>
2025-02-08 23:22:41 +0900

    refactor: 이미지 기능 관련 스웨거, DTO 제한설정

--- src/main/java/com/knu/algo_hive/post/controller/ImageController.java
+++ src/main/java/com/knu/algo_hive/post/controller/ImageController.java
@@ -28,7 +28,7 @@ public class ImageController {
     }
 
     @PostMapping(value = "/api/v1/images/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(summary = "이미지 업로드(png, jpeg 그리고 10MB 까지 업로드 가능)", description = "로그인 필요")
+    @Operation(summary = "이미지 업로드(png, jpeg 그리고 10MB 까지 업로드 가능)", description = "로그인 필요, 파일 필수, storageId 필수")
     @ApiErrorCodeExamples({ErrorCode.IMAGE_NOT_UPLOADED, ErrorCode.FILE_SIZE_EXCEEDED, ErrorCode.INVALID_FILE_TYPE, ErrorCode.IMAGE_UPLOAD_FAILED})
     public ResponseEntity<UrlResponse> getImageUrl(@ModelAttribute ImageUploadRequest request,
                                                    @AuthenticationPrincipal CustomUserDetails userDetails) {
@@ -36,7 +36,7 @@ public class ImageController {
     }
 
     @PostMapping("/api/v1/images/delete")
-    @Operation(summary = "이미지 제거(업로드 api를 통해 받는 url을 body에)", description = "로그인 필요")
+    @Operation(summary = "이미지 제거(업로드 api를 통해 받는 url을 body에)", description = "로그인 필요, url 필수")
     @ApiErrorCodeExamples({ErrorCode.INVALID_PATH_FORMAT, ErrorCode.IMAGE_DELETE_FAILED})
     public ResponseEntity<Void> deleteImage(@RequestBody ImageDeleteRequest request,
                                             @AuthenticationPrincipal CustomUserDetails userDetails) {
--- src/main/java/com/knu/algo_hive/post/dto/ImageUploadRequest.java
+++ src/main/java/com/knu/algo_hive/post/dto/ImageUploadRequest.java
@@ -1,9 +1,11 @@
 package com.knu.algo_hive.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.multipart.MultipartFile;
 
 public record ImageUploadRequest(
         MultipartFile file,
+        @NotBlank
         String storageId
 ) {
 }
