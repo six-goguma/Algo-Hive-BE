afb1255b1f26e00aad787bb4d95f2e26eb172d59
2iedo <162278702+2iedo@users.noreply.github.com>
2025-02-10 06:03:37 +0900

    feat : swagger의 api 목록 불러오는 경로인 /v3/api-docs도 spring security 경로에서 제외

--- src/main/java/com/knu/algo_hive/auth/config/SecurityConfig.java
+++ src/main/java/com/knu/algo_hive/auth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.knu.algo_hive.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -27,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         .anyRequest().authenticated())
 //                          .anyRequest().permitAll())
